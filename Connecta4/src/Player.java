7331aced0ffba2c0d7c3bad461d4f810b04f0c1d
Joan Molinas Ramon <joanmramon@gmail.com>
2017-05-10 22:42:48 +0200

    Add comments and update depth

--- Connecta4/src/Player.java
+++ Connecta4/src/Player.java
@@ -1,7 +1,3 @@
-import java.awt.*;
-import java.sql.Array;
-import java.util.ArrayList;
-
 /**
  * @author joan on 24/4/17.
  * @project Connecta4
@@ -15,7 +11,7 @@ public class Player implements Jugador {
     private Integer InfinitPositiu = Integer.MAX_VALUE;
     private Integer InfinitNegatiu = -InfinitPositiu;
     private String nom = "El puto amo";
-    private int profunditat = 5; // ha de ser par, sino empezar sin la 1 llamada sin el negativo
+    private int profunditat = 6; // ha de ser par, sino empezar sin la 1 llamada sin el negativo
 
     /**
      * Constructor
@@ -24,18 +20,22 @@ public class Player implements Jugador {
     }
 
     /**
-     * Jugador
+     * @return
      */
-
     @Override
     public String nom() {
         return this.nom;
     }
 
+    /**
+     * @param t     Tauler actual de joc
+     * @param color Color de la peça que possarà
+     * @return
+     */
     @Override
     public int moviment(Tauler t, int color) {
         int eval_act, eval_ant = InfinitNegatiu, millor_mov = 0;
-        for(int i=0; i<8; ++i){
+        for(int i=0; i < t.getMida(); ++i){
             if(t.movpossible(i)){
                 Tauler aux = new Tauler(t);
                 aux.afegeix(i, color);
@@ -51,6 +51,14 @@ public class Player implements Jugador {
         return millor_mov;
     }
 
+    /**
+     * @param t
+     * @param alfa
+     * @param beta
+     * @param nivell
+     * @param color
+     * @return
+     */
     private int min_value(Tauler t, int alfa, int beta, int nivell, int color){
         if(nivell==0 || es_guanyador(t)[0]==1 || !t.espotmoure()) return evaluarTauler(t, color);
         for(int i=0;i<8;i++){
@@ -64,6 +72,14 @@ public class Player implements Jugador {
         return beta;
     }
 
+    /**
+     * @param t
+     * @param alfa
+     * @param beta
+     * @param nivell
+     * @param color
+     * @return
+     */
     private int max_value(Tauler t, int alfa, int beta, int nivell, int color){
         if(nivell==0 || es_guanyador(t)[0]==1 || !t.espotmoure()) return evaluarTauler(t,color);
         for(int i=0;i<8;i++){
@@ -77,31 +93,19 @@ public class Player implements Jugador {
         return alfa;
     }
 
-
-
-    private float alphaBeta(Tauler t, int color, int prof, float alfa, float beta) {
-        if (prof == 0 || !t.espotmoure() || es_guanyador(t)[0] == 1){
-            return evaluarTauler(t, color);
-        }
-        for (int i = 0; i < t.getMida(); ++i) {
-            if (t.movpossible(i)) {
-                Tauler aux = new Tauler(t);
-                aux.afegeix(i, color);
-                alfa = Math.max(alfa, -alphaBeta(aux, oponent(color), prof - 1, -beta, -alfa));
-                if (beta <= alfa) {
-                    return alfa;
-                }
-            } else {
-                alfa = evaluarTauler(t, color);
-            }
-        }
-        return alfa;
-    }
-
+    /**
+     * @param jugador
+     * @return
+     */
     private int oponent(int jugador) {
         return -jugador;
     }
 
+    /**
+     * @param t
+     * @param jugador
+     * @return
+     */
     private int evaluarTauler(Tauler t, int jugador) {
         int[] guanyador = es_guanyador(t);
         if (guanyador[0] == 1) return (guanyador[1] * InfinitPositiu);
@@ -136,7 +140,7 @@ public class Player implements Jugador {
                         }
                     }
 
-                    if (moreMoves != 0) score += calculateScore(aiScore, moreMoves);
+                    if (moreMoves != 0) score += calcularPuntuacio(aiScore, moreMoves);
                     aiScore = 1;
                     blanks = 0;
                 }
@@ -159,7 +163,7 @@ public class Player implements Jugador {
                         }
                     }
                 }
-                if (moreMoves != 0) score += calculateScore(aiScore, moreMoves);
+                if (moreMoves != 0) score += calcularPuntuacio(aiScore, moreMoves);
                 aiScore = 1;
                 blanks = 0;
                 if (j >= 3) {
@@ -181,7 +185,7 @@ public class Player implements Jugador {
                             }
                         }
                     }
-                    if (moreMoves != 0) score += calculateScore(aiScore, moreMoves);
+                    if (moreMoves != 0) score += calcularPuntuacio(aiScore, moreMoves);
                     aiScore = 1;
                     blanks = 0;
                 }
@@ -205,7 +209,7 @@ public class Player implements Jugador {
                                 else break;
                             }
                         }
-                        if (moreMoves != 0) score += calculateScore(aiScore, moreMoves);
+                        if (moreMoves != 0) score += calcularPuntuacio(aiScore, moreMoves);
                         aiScore = 1;
                         blanks = 0;
                     }
@@ -231,7 +235,7 @@ public class Player implements Jugador {
                                 else  break;
                             }
                         }
-                        if (moreMoves != 0) score += calculateScore(aiScore, moreMoves);
+                        if (moreMoves != 0) score += calcularPuntuacio(aiScore, moreMoves);
                         aiScore = 1;
                         blanks = 0;
                     }
@@ -241,61 +245,24 @@ public class Player implements Jugador {
         return score;
     }
 
-    int calculateScore(int aiScore, int moreMoves){
-        int moveScore = 4 - moreMoves;
-        if(aiScore==0) return 0;
-        else if(aiScore==1) return 1*moveScore;
-        else if(aiScore==2) return 10*moveScore;
-        else if(aiScore==3) return 100*moveScore;
+    /**
+     * @param puntuacio
+     * @param moviments
+     * @return
+     */
+    int calcularPuntuacio(int puntuacio, int moviments){
+        int puntuacioMoviments = 4 - moviments;
+        if(puntuacio==0) return 0;
+        else if(puntuacio==1) return 1*puntuacioMoviments;
+        else if(puntuacio==2) return 10*puntuacioMoviments;
+        else if(puntuacio==3) return 100*puntuacioMoviments;
         else return 1000;
     }
 
-
-    /*private boolean es_guanyador(Tauler t){
-
-        for (int j = 0; j<t.getMida()-3 ; j++ ){
-            for (int i = 0; i<t.getMida(); i++){
-                if (t.getColor(i,j) == 1 && t.getColor(i,j+1) == 1 && t.getColor(i,j+2) == 1 && t.getColor(i,j+3) == 1){
-                    return true;
-                }
-                else if (t.getColor(i,j) == -1 && t.getColor(i,j+1) == -1 && t.getColor(i,j+2) == -1 && t.getColor(i,j+3) == -1){
-                    return true;
-                }
-            }
-        }
-        for (int i = 0; i<t.getMida()-3 ; i++ ){
-            for (int j = 0; j<t.getMida(); j++){
-                if (t.getColor(i,j) == 1 && t.getColor(i+1,j) == 1 && t.getColor(i+2,j) == 1 && t.getColor(i+3,j) == 1){
-                    return true;
-                }
-                else if (t.getColor(i,j) == -1 && t.getColor(i+1,j) == -1 && t.getColor(i+2,j) == -1 && t.getColor(i+3,j) == -1){
-                    return true;
-                }
-            }
-        }
-        for (int i=3; i<t.getMida(); i++){
-            for (int j=0; j<t.getMida()-3; j++){
-                if (t.getColor(i,j) == 1 && t.getColor(i-1,j+1) == 1 && t.getColor(i-2,j+2) == 1 && t.getColor(i-3, j+3) == 1) {
-                    return true;
-                }
-                else if (t.getColor(i,j) == -1 && t.getColor(i-1,j+1) == -1 && t.getColor(i-2,j+2) == -1 && t.getColor(i-3, j+3) == -1) {
-                    return true;
-                }
-
-            }
-        }
-        for (int i=3; i<t.getMida(); i++){
-            for (int j=3; j<t.getMida(); j++) {
-                if (t.getColor(i, j) == 1 && t.getColor(i - 1, j - 1) == 1 && t.getColor(i - 2, j - 2) == 1 && t.getColor(i - 3, j - 3)==1){
-                    return true;
-                }
-                else if (t.getColor(i, j) == -1 && t.getColor(i - 1, j - 1) == -1 && t.getColor(i - 2, j - 2) == -1 && t.getColor(i - 3, j - 3) == -1){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }*/
+    /**
+     * @param t
+     * @return
+     */
     private int[] es_guanyador(Tauler t){
         int[] res = {0,0};
         boolean win=false;
@@ -383,5 +350,4 @@ public class Player implements Jugador {
         }
         return res;
     }
-
 }
